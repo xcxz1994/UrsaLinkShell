71d52faf0fbad46acc47b4142b192a33cec69363
xcxz1994 <981877654@qq.com>
2018-03-22 22:06:03 -0700

    BackTest

--- test.sh
+++ test.sh
@@ -80,13 +80,14 @@
 #echo "第三个元素为: ${my_array[2]}"
 #echo "第四个元素为: ${my_array[3]}"
 
-my_array[0]=A
-my_array[1]=B
-my_array[2]=C
-my_array[3]=D
+#my_array[0]=A
+#my_array[1]=B
+#my_array[2]=C
+#my_array[3]=D
 
-echo "数组的元素为: ${my_array[*]}"
-echo "数组的元素为: ${my_array[@]}"
+#echo "数组的元素为: ${my_array[*]}"
+#echo "数组的元素为: ${my_array[@]}"
+
 
 
 
