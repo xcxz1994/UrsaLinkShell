8f50aded284e4da634c0183f9a1ca19a6e7aaf64
xcxz1994 <981877654@qq.com>
2018-03-26 02:28:09 -0700

    test0326

--- test.sh
+++ test.sh
@@ -89,4 +89,35 @@ echo "数组的元素为: ${my_array[*]}"
 echo "数组的元素为: ${my_array[@]}"
 
 
+a=10
+b=20
+
+if [ $a != $b ]
+then
+ echo "$a !=$b:a != b"
+else
+ echo "$a !=$b:a = b"
+fi
+
+if [ $a -lt 100 -a $b -gt 15 ]
+then
+ echo "$a 小于 100 且 $b 大于 15 : 返回 true"
+else
+ echo "$a 小于 100 且 $b 大于 15 : 返回 false"
+fi
+
+if [ $a -lt 100 -o $b -gt 100 ]
+then
+ echo "$a 小于 100 或 $b 大于 100 : 返回 true"
+else
+ echo "$a 小于 100 或 $b 大于 100 : 返回 false"
+fi
+
+if [ $a -lt 5 -o $b -gt 100 ]
+then
+ echo "$a 小于 5 或 $b 大于 100 : 返回 true"
+else
+ echo "$a 小于 5 或 $b 大于 100 : 返回 false"
+fi
+
 
