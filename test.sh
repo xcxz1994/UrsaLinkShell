54e8841b1b56944621f8f817446845b4a8ff2932
xcxz1994 <981877654@qq.com>
2018-03-26 19:10:39 -0700

    test032702

--- test.sh
+++ test.sh
@@ -215,7 +215,7 @@ else
  echo "$a 小于 5 或 $b 大于 100 : 返回 false"
 fi
 !
-
+:<<!
 a="abc"
 b="efg"
 
@@ -254,5 +254,57 @@ then
 else
  echo "字符串为空"
 fi
+!
+
+file="/home/chunyu/ursalinkShell/test.sh"
+if [ -r $file ]
+then
+ echo "文件可读"
+else
+ echo "文件不可读"
+fi
+
+if [ -w $file ]
+then 
+ echo "文件可写"
+else
+ echo "文件不可写"
+fi
+
+if [ -x $file ]
+then 
+ echo "文件可执行"
+else
+ echo "文件不可执行"
+fi
+
+if [ -f $file ]
+then
+ echo "文件为普通文件"
+else
+ echo "文件为特殊文件"
+fi
+
+if [ -d $file ]
+then
+ echo "文件是个目录"
+else
+ echo "文件不是个目录"
+fi
+
+if [ -s $file ]
+then
+ echo "文件不为空"
+else
+ echo "文件为空"
+fi
+
+if [ -e $file ]
+then
+ echo "文件存在"
+else
+ echo "文件不存在"
+fi
+
 
 
