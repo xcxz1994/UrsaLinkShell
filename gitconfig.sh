608f716348191f8559fdbde5b13695ebdb8c2ddb
SPring Run <chunyuchunyu2015@outlook.com>
2018-04-08 03:03:21 -0700

    shell脚本自动配置github
    shell脚本自动配置github、

---
+++ gitconfig.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+sudo apt-get install git
+
+git config --global user.name "xcxz1994"
+
+git config --global user.eamil "981877654@qq.com"
+eval $(thefuck --alias)
+
+ssh-keygen -C '981877654@qq.com' -t rsa 
+
+cd ~/.ssh
+ls
+
+if [ -e id_rsa.pub ]
+then
+ idrsa=$( cat id_rsa.pub )
+ echo "$idrsa"
+else
+ echo "id_rsa.pub file not exit!!!"
+fi
+
+sudo firefox https://github.com/settings/ssh/new
+sudo sleep 30s
+
+ssh-add   ~/.ssh/id_rsa && ssh -T git@github.com
+
+
+ 
+
+
+
+
